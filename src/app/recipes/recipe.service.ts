f4ebbe9ebe20295ba1c4023c3563fe16e4785f8c
ManojKChowdhury <manojc009@gmail.com>
2020-01-03 20:25:29 +0100

    Added routing and http service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,33 +1,39 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter, Injectable} from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
 
-  recipeSelected = new EventEmitter<Recipe>();
-
-  private recipes: Recipe[] = [
+  recipesChanged = new Subject<Recipe[]>();
+  /*private recipes: Recipe[] = [
     new Recipe('First recipe',
-      'Just a test recipe',
-      '',
+      'A Gingerbread recipe',
+      'https://i2.wp.com/www.sugarspunrun.com/wp-content/uploads/2017/12/Gingerbread-Cookie-Recipe-1-of-1-8.jpg',
       [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
+        new Ingredient('Eggs', 10),
+        new Ingredient('Bread', 1)
       ]),
     new Recipe('Second recipe',
-      'Second test recipe',
-      '',
+      'A chicken recipe',
+      'https://www.modernhoney.com/wp-content/uploads/2018/03/The-Best-Chicken-Marinade-Recipe.jpg',
       [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
+        new Ingredient('Meat', 5),
+        new Ingredient('Lime', 1)
       ])
-  ];
+  ];*/
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
   getRecipes() {
     return this.recipes.slice();
   }
@@ -38,5 +44,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
